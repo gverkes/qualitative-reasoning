7bd2af84569d416344504b2f208f45168e7ca9c0
nicola-decao <nicola.decao@studenti.unipd.it>
2016-10-17 00:25:21 +0200

    plot

--- plot.py
+++ plot.py
@@ -2,40 +2,33 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-
-def drawGraph(g):
-    G = nx.Graph()
-
-
-class NodeTest:
-
-    def __init__(self, n):
-        self.__n = n
-
-    def get_name(self):
-        return self.__n
-
-    def get_quantities(self):
-        return QuantityTest("0", -1), QuantityTest("+", 0), QuantityTest("MAX", 1)
-
-    def get_child(self):
-        return None
+graph = {
+    0: {'T': ['0', 1], 'O': ['0', 1], 'I': ['+', 1], 'child': [1, 2]},
+    1: {'T': ['+', -1], 'O': ['0', -1], 'I': ['M', 0]},
+    2: {'T': ['-', 0], 'O': ['0', 0], 'I': ['+', 1]}
+}
 
 
-class QuantityTest:
+def status_to_str(status):
+    def dev_to_str(dev):
+        if dev > 0:
+            return '+'
+        elif dev < 0:
+            return '-'
+        else:
+            return '0'
 
-    def __init__(self, v, d):
-        self.__v = v
-        self.__d = d
+    return'\n'.join((k + ':(' + str(v[0]) + ',' + dev_to_str(v[1]) + ')' for k, v in sorted(status.items()) if k != 'child'))
 
-    def get_values(self):
-        return "0", "+", "MAX"
 
-    def get_derivatives(self):
-        return "+", "0", "-"
+def drawGraph():
+    G = nx.DiGraph()
+    G.add_nodes_from(graph.keys())
+    G.add_edges_from(((k, c) for k, v in graph.items() if 'child' in v for c in v['child']))
+    nx.draw_networkx(G, node_shape='s', node_size=2500, labels={k: status_to_str(v) for k, v in graph.items()})
 
-    def get_val(self):
-        return self.__v
+    plt.axis('off')
+    plt.savefig('fig')
+    plt.show()
 
-    def get_dev(self):
-        return self.__d
+drawGraph()
